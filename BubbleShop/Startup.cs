347d4e6b6eeb62e63016602c2b8ed4d0303ad3a8
Ares Tseng <ares.tseng@getac.com.tw>
2018-06-21 16:56:18 +0800

    First commit

---
+++ BubbleShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BubbleShop.Startup))]
+namespace BubbleShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
